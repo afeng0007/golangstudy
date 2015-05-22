21b0680354411641a5bf28ed67472fdf28e78611
afeng <afeng0007@gmail.com>
2015-05-22 09:24:53 +0800

    add more example at https://gobyexample.com/

--- constants.go
+++ constants.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 import "math"
 
-const s string="constants"
+const s string = "constants"
 
-func main(){
-	fmt.Println(s);
-	const n=5000000;
-	fmt.Println(n);
-	const d=3e20/n;
-	fmt.Println(d);
-	fmt.Println(int64(d));
-	fmt.Println(math.Sin(n));
+func main() {
+	fmt.Println(s)
+	const n = 5000000
+	fmt.Println(n)
+	const d = 3e20 / n
+	fmt.Println(d)
+	fmt.Println(int64(d))
+	fmt.Println(math.Sin(n))
 }
